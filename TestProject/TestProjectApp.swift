aa575f8c54ea93f419ff48625ccb003f6942f016
BT_C09_25 <mobileappdev@bths.edu>
2026-04-28 08:57:47 -0400

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Student on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
