a52b453517b51584b0a641f976ffcb7ed6c8bc74
BT_C09_25 <mobileappdev@bths.edu>
2026-04-29 09:09:54 -0400

    Changed code

--- TestProject/ContentView.swift
+++ TestProject/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "bolt")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Test text")
         }
         .padding()
     }
